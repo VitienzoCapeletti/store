353a1c62f2ce7ad1c2e34641416220142c40ebaf
Vitenzo <vitienzodias@gmail.com>
2021-03-16 16:28:03 -0300

    Service layer, componet registration

---
+++ src/main/java/com/vitienzo/store/services/UserService.java
@@ -0,0 +1,25 @@
+package com.vitienzo.store.services;
+
+import com.vitienzo.store.entities.User;
+import com.vitienzo.store.repositories.UseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UseRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
