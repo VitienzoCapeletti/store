280a54669a3edb1eba20b1cb87c373d333e95c66
Vitenzo <vitienzodias@gmail.com>
2021-03-26 16:14:55 -0300

    Excpetion handling - findById

---
+++ src/main/java/com/vitienzo/store/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.vitienzo.store.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+
+    }
+}
+
